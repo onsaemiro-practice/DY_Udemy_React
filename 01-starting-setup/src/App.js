2724b3769068f162757cad5d08f7c17cf32176f8
dongyoon7212 <dongyoon7212@naver.com>
2023-04-21 19:52:01 +0900

    useCallback() 및 해당 종속성

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,23 +1,33 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Button from "./components/UI/Button/Button";
 import "./App.css";
 import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("APP RUNNING");
 
-  const toggleParagraphHandler = () => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, []);
+  // useCallback() : 함수를 리액트의 내부 공간에 저장해서 함수 객체가 실행될때마다 이를 재사용할 수 있게 해줌
+  // 두번째 인자는 배열이여야만 한다. useCallback 호출에 대한 의존성 배열이다.
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
   };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
       {/* props값이 고정되어있더라도 인자값을 받는 컴포넌트와 그 자식 컴포넌트들은
       항상 재평가됨. */}
+      <Button onClick={allowToggleHandler}>Allow Topggling</Button>
       <Button onClick={toggleParagraphHandler}>Show Paragraph</Button>
     </div>
   );
